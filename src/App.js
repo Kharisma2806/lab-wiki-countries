2a48040bac9c6aaaa57e432e1be1b2a408f13318
Kharisma <kiran.nankani28@gmail.com>
2023-06-11 14:28:14 +0200

    solved lab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import CountriesList from "./components/CountriesList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CountryDetails from "./components/CountryDetails";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countriesArr, setcountriesArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then(response => {
+        const countries = response.data;
+        setcountriesArr(countries);
+        console.log(countriesArr);
+      })
+      .catch(error => {
+        console.error("Error fetching countries:", error);
+      });
+  }, [countriesArr]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <div className="App-headerpp">
+        <Navbar />
+      </div>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-3"
+            style={{ maxHeight: "90vh", overflow: "scroll" }}
+          >
+            <CountriesList countries={countriesArr} />
+          </div>
+          <div className="col-9">
+            <Routes>
+              <Route
+                path="/:id"
+                element={<CountryDetails countries={countriesArr} />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
